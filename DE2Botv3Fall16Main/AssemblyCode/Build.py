19254bcabe8957dc36f9a0dbb078eebe56d08a07
Harrison Statham <harrison.statham@comcast.net>
2016-11-17 20:05:59 -0500

    Changed the build script to python. Its a little easier to manage.

--- DE2Botv3Fall16Main/AssemblyCode/Build.py
+++ DE2Botv3Fall16Main/AssemblyCode/Build.py
@@ -1,11 +1,58 @@
+#
+# Build File
+#
+#
 
-#
-#
-#
-#
+inputFiles = [
+	
+	
+	'Include\ProgramHeader.asm',
+	'Include\Init\InterruptTable.asm',
+	'Include\Init\Initialization.asm',
+	'Include\Init\WaitForSafety.asm',
+	'Include\Init\WaitForUser.asm',
+
+	'Main.asm',
+
+	'Include\SubRoutines\Abs.asm',
+	'Include\SubRoutines\Atan2.asm',
+	'Include\SubRoutines\BattCheck.asm',
+	'Include\SubRoutines\CTimer_ISR.asm',
+	'Include\SubRoutines\Div16s.asm',
+	'Include\SubRoutines\I2C.asm',
+	'Include\SubRoutines\L2Estimate.asm',
+	'Include\SubRoutines\Mod360.asm',
+	'Include\SubRoutines\Mult16s.asm',
+	'Include\SubRoutines\Wait1.asm',
+
+	'Include\Static\Variables.asm',
+	'Include\Static\Constants.asm',
+	'Include\Static\MemoryAddresses.asm'
+]
+
+
+outputFile = 'FinalProgram.asm'
+
 
 def main():
-	pass
+	
+	with open(outputFile, 'a') as outFile:
+
+		for f in inputFiles:
+
+			inFile 	= open(f, 'r')
+			data 	= inFile.read()
+
+			outFile.write(data)
+
+			inFile.close()
+
+
+	
+
+
+
+
 
 
 if __name__ == '__main__':
